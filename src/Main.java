b34a4b10b05a29cbcbc7a03d84a909e47a965706
jibekabdykerim <254720@astanait.edu.kz>
2026-04-28 19:52:07 +0500

    init

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Experiment lab = new Experiment();
-        System.out.println("Algorithm Performance");
+        System.out.println("Algorithm Performance:");
         lab.runAllExperiments();
     }
 }
